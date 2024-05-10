c61faa66dbbed05106f94132613328f751f675bc
hstarorg <hm910705@163.com>
2024-05-11 00:42:23 +0800

    chore: configure vercel deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
+import { join } from 'node:path';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+import { ALL_CONTROLLERS } from './controllers';
+import { INJECTED_SERVICES } from './services';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, './web'),
+      exclude: ['/api/(.*)'],
+    }),
+  ],
+  controllers: ALL_CONTROLLERS,
+  providers: [...INJECTED_SERVICES],
 })
 export class AppModule {}
---
+++ src/services/index.ts
@@ -0,0 +1,5 @@
+import { AppService } from './app.service';
+
+export { AppService };
+
+export const INJECTED_SERVICES = [AppService];
