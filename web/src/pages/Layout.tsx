23ad78906bf9cbb1bb1096b2dbe0b011c9931477
hstarorg <hm910705@163.com>
2024-05-11 23:49:50 +0800

    feat: support logo click

--- web/src/pages/Layout.tsx
+++ web/src/pages/Layout.tsx
@@ -1,10 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 export function Layout() {
+  const navigate = useNavigate();
+
+  function handleLogoClick() {
+    navigate('/');
+  }
+
   return (
     <>
       <header className="h-16 py-2 px-4 sticky shadow-md bg-slate-900/5 backdrop-blur top-0 z-50">
-        <div className="flex ">
+        <div className="flex cursor-pointer" onClick={handleLogoClick}>
           <img src="wt-logo-min.png" className="h-12 w-12" alt="" />
           <div className="ml-2 leading-[48px] text-2xl font-bold">
             Web3 Tools
