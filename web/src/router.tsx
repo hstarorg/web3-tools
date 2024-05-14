c5a04217f1a15341c9e3c7cb1a67969e093fd48c
hstarorg <hm910705@163.com>
2024-05-14 22:45:05 +0800

    fix: tools home page is empty when request /tools

--- web/src/router.tsx
+++ web/src/router.tsx
@@ -5,6 +5,7 @@ import { Layout } from './pages/Layout';
 import { HomePage } from './pages/Home';
 
 import { ToolsLayout } from './pages/tools/Layout';
+import { ToolsHome } from './pages/tools/ToolsHome';
 import { ExchangeTools } from './pages/tools/ExchangeTools';
 import { EvmDevTools } from './pages/tools/EvmDevTools';
 
@@ -19,6 +20,7 @@ export const router = createBrowserRouter([
         path: '/tools',
         element: <ToolsLayout />,
         children: [
+          { path: '/tools', element: <ToolsHome /> },
           { path: '/tools/exchange', element: <ExchangeTools /> },
           { path: '/tools/evmdev', element: <EvmDevTools /> },
           { path: '*', element: <NotFound /> },
