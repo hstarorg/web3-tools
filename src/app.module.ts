283f6aa2ab2b07b9eb46c024b1e9caf8e34aead6
hstarorg <hm910705@163.com>
2025-03-04 21:52:46 +0800

    refactor: update nestjs framework

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,16 @@ import { ensureDirSync } from 'fs-extra';
 import { ALL_CONTROLLERS } from './controllers';
 import { INJECTED_SERVICES } from './services';
 
-const webAppDir = join(__dirname, './web');
+// const webAppDir = join(__dirname, './web');
+const webAppDir = join(__dirname, '../src/web');
 ensureDirSync(webAppDir);
+console.log(`web app dir: ${webAppDir}`);
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: webAppDir,
-      exclude: ['/api/(.*)'],
+      exclude: ['/api/*all'],
     }),
   ],
   controllers: ALL_CONTROLLERS,
